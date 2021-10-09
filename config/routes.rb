5abb6a0a0346f0d578a362921c2712a5489bd28e
Martin Krause <findmemart@gmail.com>
2021-10-09 16:45:51 +0200

    watch-list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  get "lists", to: "lists#index"
+  get "lists/:id", to: "lists#show", as: :list
+
+  get "lists/new", to: "lists#new"
+  post "lists", to: "lists#create"
+
+  delete "lists/:id", to: "lists#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
